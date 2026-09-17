108732c394c62f9308100e1851071fc7c42f2e05
María Cecilia <lunama.cecilia@live.com>
2026-09-17 19:54:17 -0300

    estructura de funciones con parámetros

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+
+//PARÁMETROS
+const numeroUno = 34;
+const numeroDos = 45;
+
+const suma = (a, b) => {
+    const sumaNumeros = a + b;
+    console.log(sumaNumeros);
+}
+
+const login = (email, password) => {
+ const usuarioLogueado = email + " " + password;
+ console.log(usuarioLogueado)
+}
